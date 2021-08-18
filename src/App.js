639dc053b8c147094ebcc60101c9bea3aef5cf4e
Pandula <43129749+p4nduxcv@users.noreply.github.com>
2021-08-18 13:04:41 +0530

    providers and useSelector Hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const state = useSelector((state) => state);
+  console.log(state);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello Pandula's CodeSandbox</h1>
+      <h2>Start Redux</h2>
     </div>
   );
 }
